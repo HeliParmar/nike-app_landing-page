0e6913513b1d85e420c6c5a0861870f679795a4c
HeliParmar <142568224+HeliParmar@users.noreply.github.com>
2024-04-13 08:34:42 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import PopolarProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpacialOffer from './SpacialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopolarProducts,
+    SuperQuality,
+    Services,
+    SpacialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+    
+}
